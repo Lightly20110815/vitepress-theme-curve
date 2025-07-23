99b3a3a81e0755e5acf5afba629c2ff288a004f4
Lightly20110815 <yangxingran2021@outlook.com>
2025-07-23 19:45:16 +0800

    66366

--- .vitepress/theme/views/About.vue
+++ .vitepress/theme/views/About.vue
@@ -5,8 +5,8 @@
       <!-- 介绍 -->
       <div class="about-item hello">
         <span class="text1">你好，很高兴认识你👋</span>
-        <span class="text2 title2">我是 池鱼</span>
-        <span class="text3">是一名 正在HRT的MtF🏳‍⚧🏳‍⚧</span>
+        <span class="text2 title2">我是 Sy Yann</span>
+        <span class="text3">是一名,,,,,算了我也不知道?</span>
       </div>
       <!-- 追求 -->
       <!--
